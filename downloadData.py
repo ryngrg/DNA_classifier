c837da5df61008a01128e3df2f50838eb21b196d
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2021-07-05 18:55:06 -0400

    Update downloadData.py
    Now, the program downloads all sequence reads for all samples at http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/

--- downloadData.py
+++ downloadData.py
@@ -1,12 +1,63 @@
 import requests
+import os
+import sys
 
 url_base = "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/"
 
-sample = "HG00637"
-filename = "SRR070520"
+def get_all_links(url):
+    """This function returns a list of all links in the webpage
+    Eg:- get list of all samples, or list of all files for a sample
+    NOTE: only works for these particular webpages only, it is hardcoded
+    """
+    links = []
+    try:
+        webPage = requests.get(url).text
+    except requests.exceptions.RequestException as e:
+        print("[Exception]:\n", e)
+        return []
+    allinvs = webPage.split('"')
+    for i in range(3, len(allinvs)):
+        if i % 2 == 1:
+            links += [allinvs[i]]
+    return links
 
-url = url_base + sample + "/sequence_read/" + filename + ".filt.fastq.gz"
+def download_file(sample, filename):
+    """This function downloads the file asked for
+    and saves it in the path:
+        ./phase3_data/sampleName/file
+    """
+    global url_base
+    url = url_base + sample + "sequence_read/" + filename
+    try:
+        myfile = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        print("Download failed for flie:", filename)
+        print("[Exception]:", e)
+        return None
+    open("./phase3_data/" + sample + filename, 'wb').write(myfile.content)
 
-myfile = requests.get(url)
+def download_all_data():
+    """This is the main function.
+    It downloads and saves all phase 3 sequence reads for all samples
+    """
+    global url_base
+    num_downloads = 0
+    samples = get_all_links(url_base)
 
-open("./phase3_data/" + sample + '/' + filename + ".filt.fastq.gz", 'wb').write(myfile.content)
+    for sample in samples:
+        print("Sample:", sample)
+        # create new directory for the sample
+        try:
+            os.mkdir("./phase3_data/" + sample)
+        except FileExistsError:
+            print("Folder for sample already exists")
+        files = get_all_links(url_base + sample + "sequence_read/")
+        for file in files:
+            print("-> downloading file:", file)
+            # downloads file onto your computer
+            download_file(sample, file)
+            num_downloads += 1
+
+    print("Successfully downloaded", str(num_downloads), "files for", str(len(samples)), "samples.")
+
+download_all_data()
