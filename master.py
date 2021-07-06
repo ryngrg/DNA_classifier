df23a33f9b45d7aa4173d002bec29c754ecf1de6
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2021-07-06 00:19:25 -0400

    Update master.py
    call the parser

--- master.py
+++ master.py
@@ -1,3 +1,10 @@
 from downloadData import download_all_data
+from parseSequence import parseAllFastqs
 
+# download files
 samples, files = download_all_data()
+
+# call function for unzipping files here
+
+# parse and save files as .txt
+parseAllFastqs(samples, files)
