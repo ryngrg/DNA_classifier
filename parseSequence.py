46012d1ff8538a730480701128d5c4d97c20440a
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2021-07-06 00:17:12 -0400

    Update parseSequence.py
    Now, the parser runs on all files for all samples

--- parseSequence.py
+++ parseSequence.py
@@ -1,22 +1,32 @@
-sample = "HG00637"
-filename = "SRR070520"
+def parse_file(sample, filename):
+    """This function converts a .fastq file into a .txt file
+    while doing so, it only retains the raw sequence reads
+    """
+    try:
+        f = open("./phase3_data/" + sample + filename, 'r')
+    except:
+        print("[Error]: Could not access file:", filename, "for sample:", sample[:-1])
+    outfile = open("./phase3_data/" + sample + filename[:-11] + ".txt", 'w')
+    useNext = False
+    while True:
+        text = f.readline()
+        if len(text) == 0:
+            break
+        if text[0] == '+':
+            useNext = False
+        if useNext and (text[0] != '@'):
+            outfile.write(text)
+        if text[0] == '@':
+            useNext = True
+    f.close()
+    outfile.close()
 
-f = open("./phase3_data/" + sample + '/' + filename + ".filt.fastq", 'r')
-
-outfile = open("./phase3_data/" + sample + '/' + filename + ".txt", 'w')
-
-useNext = False
-
-while True:
-    text = f.readline()
-    if len(text) == 0:
-        break
-    if text[0] == '+':
-        useNext = False
-    if useNext and (text[0] != '@'):
-        outfile.write(text)
-    if text[0] == '@':
-        useNext = True
-
-f.close()
-outfile.close()
+def parseAllFastqs(samples, all_files):
+    """This is the main function
+    It calls the parser on every file for all samples
+    """
+    for i in range(len(all_files)):
+        sample = samples[i]
+        for file in all_files[i]:
+            print("parsing file:", file)
+            parse_file(sample, file[:-3])
