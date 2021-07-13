4d29a3a8d038a21772af575e82a3489789f595d0
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2021-07-12 23:58:51 -0400

    Update master.py

--- master.py
+++ master.py
@@ -3,9 +3,14 @@ from unzipAllGz import unzipAll
 from parseSequence import parseAllFastqs
 from trainingData import prepData
 
-## Get all data
+## Get data
+
+samplesRange = (0, 4) # use first four samples only
+
 # download files *.filt.fastq.gz
-samples, files = download_all_data()
+samples, files = download_all_data(samplesRange)
+print(samples)
+print(files)
 # unzip *.filt.fastq.gz into *.filt.fastq
 unzipAll(samples, files)
 # parse and save *.filt.fastq as *.txt
