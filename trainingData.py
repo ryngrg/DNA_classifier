4089328556abacc42b3d78ee18ff50599b819676
Aaryan Garg <73568701+ryngrg@users.noreply.github.com>
2021-07-13 11:49:01 -0400

    Update trainingData.py

--- trainingData.py
+++ trainingData.py
@@ -3,24 +3,31 @@ import numpy as np
 def prepData(samples, all_files):
     x = []
     y = []
+    decoding = [(1, 0, 0, 0),
+                (0, 1, 0, 0),
+                (0, 0, 1, 0),
+                (0, 0, 0, 1)]
     for i in range(len(all_files)):
         sample = samples[i]
         popgp = get_y_sample(sample)
         for file in all_files[i]:
             y += [popgp]
-            f = open("./phase3_data/" + sample + file[:-14] + ".txt", 'r')
-            raw = f.read()
+            f = open("./phase3_data/" + sample + file[:-14] + ".bin", 'rb')
+            raw = list(f.read())
             f.close()
             ohvs = []
             for t in range(len(raw)):
-                if raw[t] == 'A':
-                    ohvs += [(1, 0, 0, 0)]
-                elif raw[t] == 'C':
-                    ohvs += [(0, 1, 0, 0)]
-                elif raw[t] == 'G':
-                    ohvs += [(0, 0, 1, 0)]
-                elif raw[t] == 'T':
-                    ohvs += [(0, 0, 0, 1)]
+                base = (int)(raw[t] / 4**3)
+                ohvs += [decoding[base]]
+                raw[t] %= (4**3)
+                base = (int)(raw[t] / 4**2)
+                ohvs += [decoding[base]]
+                raw[t] %= (4**2)
+                base = (int)(raw[t] / 4**1)
+                ohvs += [decoding[base]]
+                raw[t] %= (4**1)
+                base = (int)(raw[t])
+                ohvs += [decoding[base]]
             x += [ohvs]
     X = np.array(x)
     Y = np.array(y)
