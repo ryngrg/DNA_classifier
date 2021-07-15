2b9535d20020c78e8cee433add4c8d7c0d307a9b
Sparsh Gupta <sparshg@gmail.com>
2021-07-15 10:56:49 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -6,7 +6,9 @@ import matplotlib as plt
 def make_model():
     model = keras.Sequential()
     model.add(keras.layers.LSTM(64, return_sequences=True, input_shape=(None, 4)))
+    model.add(keras.layers.Dropout(0.3))
     model.add(keras.layers.LSTM(10, return_sequences=False))
+    model.add(keras.layers.Dropout(0.3))
     model.add(keras.layers.Dense(5, activation = tf.nn.softmax))
     model.summary()
     model.compile(optimizer = keras.optimizers.Adam(), loss = keras.losses.CategoricalCrossentropy())
